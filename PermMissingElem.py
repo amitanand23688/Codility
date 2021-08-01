16404a1ea4f71ecd969e396dd91e32629e6cfdf9
amitanand23688 <88266426+amitanand23688@users.noreply.github.com>
2021-08-01 13:23:52 +0530

    Update PermMissingElem.py

--- PermMissingElem.py
+++ PermMissingElem.py
@@ -27,3 +27,16 @@ Write an efficient algorithm for the following assumptions:
 
 
 '''
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    if len(A)>0:
+        if len(A) > 1:
+            A.sort() 
+        for i in range(0,len(A)):
+            if A[i] != i+1:
+                return i+1
+    else:
+        return 0
