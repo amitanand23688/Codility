68f37bfaefc43383f6774a12d8083cf3c905eff6
amitanand23688 <88266426+amitanand23688@users.noreply.github.com>
2021-08-01 13:04:12 +0530

    Update FrogJump.py

--- FrogJump.py
+++ FrogJump.py
@@ -28,3 +28,12 @@ Write an efficient algorithm for the following assumptions:
         X ≤ Y.
 
 '''
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X, Y, D):
+    # write your code in Python 3.6
+    if X < Y:
+        return ((Y-X) // D ) + 1
+    else:
+        return 0
